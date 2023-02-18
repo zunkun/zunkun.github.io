e205cfbe47b7bdbb2b43f87a06c1f43e0ae7909e
zunkun <liuzunkun@aliyun.com>
2023-02-18 19:02:06 +0800

    初次提交部署

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,19 @@
+export default {
+  title: '得時笔记',
+  description: '记录工作，计算机技术',
+  themeConfig: {
+    siteTitle: '得時笔记',
+    nav: [
+      { text: '网络安全', link: '/网络安全/' },
+      {
+        text: '前端笔记',
+        items: [
+          { text: 'JavaScript', link: '/javascript' },
+          { text: 'Item B', link: '/item-2' },
+          { text: 'Item C', link: '/item-3' },
+        ],
+      },
+      { text: '基本技能', link: '/基本技能/' },
+    ],
+  },
+};
