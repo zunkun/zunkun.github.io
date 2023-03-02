06afc48b89e35f0bd1769a0bc74768b0333b856b
zunkun <liuzunkun@aliyun.com>
2023-03-02 20:37:06 +0800

    feat: 首页分页加载

--- docs/.vitepress/theme/PageList.vue
+++ docs/.vitepress/theme/PageList.vue
@@ -1,19 +1,47 @@
 <template>
   <div class="pages">
-   <div class="pageitem" v-for="pageInfo in props.list" :key="pageInfo.title">
+   <div class="pageitem" v-for="pageInfo in pageList.list" :key="pageInfo.title">
      <div class="title"><a :href="pageInfo.link">{{ pageInfo.title }}</a></div>
      <div class="subs">
       <div class="subitem">{{ pageInfo.date }}</div>
      </div>
    </div>
-
+   <div class="moreline" v-if="pageList.hasMore">
+      <div class="morebox" @click="onLoad">加载更多...</div>
+   </div>
   </div>
  </template>
 
 <script setup>
+import { reactive } from 'vue';
+
 const props = defineProps({
   list: Array,
 });
+
+console.log(3, 4, 5);
+
+const pageList = reactive({
+  list: [],
+  page: 0,
+  size: 10,
+  hasMore: true,
+});
+
+function onLoad() {
+  const total = props?.list?.length || 0;
+
+  if (pageList.list.length < total) {
+    pageList.page += 1;
+    pageList.list = props?.list?.slice(0, 10 * pageList.page);
+  }
+
+  if (pageList.list.length >= total) {
+    pageList.hasMore = false;
+  }
+}
+
+onLoad();
 
 </script>
 
@@ -37,6 +65,20 @@ const props = defineProps({
         font-size: 14px;
         line-height: 20px;
       }
+    }
+  }
+
+  .moreline {
+    text-align: center;
+    cursor: pointer;
+
+    .morebox {
+      display: inline-block;
+      width: 200px;
+      height: 30px;
+      font-size: 20px;
+      // border: 1px solid green;
+
     }
   }
 }
