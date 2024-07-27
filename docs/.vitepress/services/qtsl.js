cd8b75bea46129fae589bc04084206748d59247a
zunkun <liuzunkun@aliyun.com>
2024-07-27 14:54:11 +0800

    未注标点诗歌列表

--- docs/.vitepress/services/qtsl.js
+++ docs/.vitepress/services/qtsl.js
@@ -1,4 +1,4 @@
-import { mkdirSync, writeFileSync, readdirSync, link } from 'fs';
+import { mkdirSync, writeFileSync, readdirSync, existsSync } from 'fs';
 import { getDateStr } from './utils';
 import qtslchapters from './qtslchapters';
 
@@ -54,6 +54,33 @@ import authorMap from '/data/qtsl/${chapter}/author.json'
 `;
 
     writeFileSync(filePath, content, 'utf-8');
+
+    const notMarkedFilePath = `docs/data/qtsl/${chapter}/poems.notmarked.json`;
+    // 处理未标注标点的诗句
+    const exist = existsSync(notMarkedFilePath);
+
+    if (!exist) return;
+
+    const title2 = `御定全唐詩錄${chapter}未标注标点诗歌列表`;
+    const filePath2 = `${chapterDir}/notmarked.md`;
+    const content2 = `---
+title: ${title2}
+date: '${dateStr}'
+udate: '${dateStr}'
+aside: false
+---
+# ${title2}
+
+<PoemList :list="poems" :authorMap="authorMap" :chapternum="${chapternum}" />
+
+<script setup>
+const chapter = '${chapter}';
+import poems from '/data/qtsl/${chapter}/poems.notmarked.json'
+import authorMap from '/data/qtsl/${chapter}/author.json'
+</script>
+`;
+
+    writeFileSync(filePath2, content2, 'utf-8');
   },
 
   lineFileCreate(chapterDir, chapter, chapternum) {
@@ -115,24 +142,36 @@ import authorMap from '/data/qtsl/${chapter}/author.json'
         const chapter = fileName;
         const chapternum = parseInt(qtslchapters.indexOf(chapter), 10) + 1;
 
+        const subitems = [
+          {
+            text: `${chapter}诗歌列表`,
+            link: `${prePath}chapter${chapternum}/poem`,
+          },
+          {
+            text: `${chapter}按行分析`,
+            link: `${prePath}chapter${chapternum}/line`,
+          },
+          {
+            text: `${chapter}诗人作者`,
+            link: `${prePath}chapter${chapternum}/author`,
+          },
+        ];
+
+        const notMarkedFilePath = `docs/data/qtsl/${chapter}/poems.notmarked.json`;
+        // 处理未标注标点的诗句
+        const exist = existsSync(notMarkedFilePath);
+        if (exist) {
+          subitems.push({
+            text: '未注标点诗歌',
+            link: `${prePath}chapter${chapternum}/notmarked`,
+          });
+        }
+
         const chapterItem = {
           chapternum,
           text: chapter,
           collapsed: true,
-          items: [
-            {
-              text: `${chapter}诗歌列表`,
-              link: `${prePath}chapter${chapternum}/poem`,
-            },
-            {
-              text: `${chapter}按行分析`,
-              link: `${prePath}chapter${chapternum}/line`,
-            },
-            {
-              text: `${chapter}诗人作者`,
-              link: `${prePath}chapter${chapternum}/author`,
-            },
-          ],
+          items: subitems,
         };
 
         items.push(chapterItem);
