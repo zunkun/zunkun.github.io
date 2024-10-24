81b14882958ae453de7819e0b94df17c79c99f7d
zunkun <liuzunkun@aliyun.com>
2024-10-24 21:41:55 +0800

    css

--- docs/.vitepress/theme/GroupList.vue
+++ docs/.vitepress/theme/GroupList.vue
@@ -25,13 +25,13 @@ const props = defineProps({
 .itembox {
     cursor: pointer;
     border-radius: 4px;
-    border: 1px solid #99CC99;
+    border: 1px solid var(--vp-c-brand-1);
 
     text-align: center;
     font-size: 20px;
     line-height: 50px;
 
-    box-shadow:  0px 1px 4px #10b981 inset;
+    // box-shadow:  0px 1px 4px var(--vp-c-brand-1);
 
     a {
       display: block;
